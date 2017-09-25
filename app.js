92180d0fd21823383d246f35b84317e5d2eb68d3
DESKTOP-2I4RNGJ\Kembly <hern.suwannasam@gmail.com>
2017-09-25 17:56:59 +0700

    v1

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express')
 var cors = require('cors')
 var request = require('request');
 var http = require("http");
-const app = express()
+const app = express();
 
 app.use(cors())
 
 var port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-  request('https://api.instagram.com/v1/users/self/media/recent/?access_token=2461863410.05b0267.35ab4c7c1f234a6282d8fc1b96318d8b&count=12', function (error, response, body) {
+  request('https://api.instagram.com/v1/users/self/media/recent/?access_token=2461863410.05b0267.35ab4c7c1f234a6282d8fc1b96318d8b&count=8', function (error, response, body) {
     res.send(body);
   });
 })
